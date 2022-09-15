da87441cfae65059213277a6c19bc436e9b29633
MoniVasileva <monivasileva2306@gmail.com>
2022-09-15 14:25:15 +0300

    task 1

--- Data-Definition-And-Datatypes/demo.sql
+++ Data-Definition-And-Datatypes/demo.sql
@@ -1,6 +1,16 @@
-CREATE TABLE gamebar.people(
-                               id INT auto_increment primary key,
-                               first_name varchar(100) not null,
-   last_name varchar(100) NOT NULL
+CREATE DATABASE gamebar;
 
+CREATE TABLE gamebar.employees (
+                                   id INT auto_increment primary key,
+                                   first_name varchar(100) not null,
+                                   last_name varchar(100) NOT NULL
+);
+CREATE TABLE gamebar.categories (
+                                    id INT auto_increment primary key,
+                                    name varchar(100) not null
+);
+CREATE TABLE gamebar.products (
+                                  id INT auto_increment primary key,
+                                  name varchar(100) not null,
+                                  category_id INT NOT NULL
 );
