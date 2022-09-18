68eff3551c30be3f6014f16e9985389a868ba3c9
MoniVasileva <monivasileva2306@gmail.com>
2022-09-18 15:22:26 +0300

    Exercise: Data Definition and Datatypes

--- Data-Definition-And-Datatypes/exercise.sql
+++ Data-Definition-And-Datatypes/exercise.sql
@@ -89,4 +89,15 @@ ALTER TABLE users
     ADD CONSTRAINT pk_users PRIMARY KEY users (id),
     MODIFY COLUMN user_name VARCHAR(30) UNIQUE;
 
+CREATE DATABASE soft_uni;
+USE soft_uni;
+CREATE TABLE towns(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+CREATE TABLE addresses(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    address_text VARCHAR(255) NOT NULL ,
+    town_id INT NOT NULL
+);
 
